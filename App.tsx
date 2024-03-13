40c5dacbdec526d156cd3c1304a1273d541f6a9d
prateek041 <prateeksingh9741@gmail.com>
2024-03-13 23:11:58 +0530

    Update imports and add background health data sync

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import {
   initialize,
   requestPermission,
   insertRecords,
-} from 'react-native-health-connect';
-import { StepsRecord } from 'react-native-health-connect/lib/typescript/types';
+} from "react-native-health-connect";
+import { StepsRecord } from "react-native-health-connect/lib/typescript/types";
+import {
+  registerBackgroundFetchAsync,
+  unregisterBackgroundFetchAsync,
+  checkIsRegistered,
+  checkStatusAsync,
+} from "./backgroud/healthDataSync";
 
 const insertStepsCount = async () => {
   const now = new Date();
@@ -17,7 +24,7 @@ const insertStepsCount = async () => {
   const endTime = startTimeObject.toISOString();
 
   const steps: StepsRecord = {
-    recordType: 'Steps',
+    recordType: "Steps",
     count: 500,
     startTime,
     endTime,
@@ -27,35 +34,61 @@ const insertStepsCount = async () => {
   return insertedRecords;
 };
 
+const getHealthData = async () => {
+  const isInitialized = await initialize();
+  if (!isInitialized) {
+    console.log("Failed to initialize");
+  }
+
+  const grantedPermissions = await requestPermission([
+    {
+      accessType: "read",
+      recordType: "Steps",
+    },
+    {
+      accessType: "write",
+      recordType: "Steps",
+    },
+  ]);
+
+  const insertedRecords = await insertStepsCount();
+  console.log("Inserted records", insertedRecords);
+};
+
 export default function App() {
+  const [isRegistered, setIsRegistered] = useState(false);
+
   useEffect(() => {
-    const getHealthData = async () => {
-      const isInitialized = await initialize();
-      if (!isInitialized) {
-        console.log('Failed to initialize');
-      }
-
-      const grantedPermissions = await requestPermission([
-        {
-          accessType: 'read',
-          recordType: 'Steps',
-        },
-        {
-          accessType: 'write',
-          recordType: 'Steps',
-        },
-      ]);
-
-      const insertedRecords = await insertStepsCount();
-      console.log('Inserted records', insertedRecords);
-    };
-
-    getHealthData();
+    // getHealthData();
+    check();
   }, []);
+
+  const check = async () => {
+    const isRegistered = await checkIsRegistered();
+    setIsRegistered(isRegistered);
+  };
+
+  const toggleFetchTask = async () => {
+    if (isRegistered) {
+      await unregisterBackgroundFetchAsync();
+    } else {
+      await registerBackgroundFetchAsync();
+    }
+
+      check();
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
+      <Text>Open up App.tsx to start working on your app I don't know</Text>
       <StatusBar style="auto" />
+      <Button
+        title={
+          isRegistered
+            ? "Unregister BackgroundFetch task"
+            : "Register BackgroundFetch task"
+        }
+        onPress={toggleFetchTask}
+      />
     </View>
   );
 }
@@ -63,8 +96,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ backgroud/healthDataSync.ts
@@ -0,0 +1,42 @@
+import * as BackgroundFetch from "expo-background-fetch"
+import * as TaskManager from "expo-task-manager"
+
+
+// How this is going to be implemented
+// 1: fetch data from health connect every 5 minutes
+// 2: store the data in a local place
+// 3: every 30 minutes, check compare the data from last sync
+// 4: if the data is different, aggregate it on the basis of types of Data
+// 5: make data type specific http calls to the server
+const HEALTH_DATA_SYNC_TASK = "health-data-sync"
+
+TaskManager.defineTask(HEALTH_DATA_SYNC_TASK, async () => {
+    const now = new Date()
+
+    console.log("Get Background Fetch Data", now.toISOString())
+
+    console.log(BackgroundFetch.BackgroundFetchResult)
+    return BackgroundFetch.BackgroundFetchResult.NewData
+})
+
+// register the task
+export async function registerBackgroundFetchAsync() {
+    console.log("I was registered man")
+    return BackgroundFetch.registerTaskAsync(HEALTH_DATA_SYNC_TASK, {
+        minimumInterval: 1 * 60, // 5 minutes
+        stopOnTerminate: false,
+        startOnBoot: true,
+    })
+}
+
+export async function unregisterBackgroundFetchAsync() {
+    return BackgroundFetch.unregisterTaskAsync(HEALTH_DATA_SYNC_TASK)
+}
+
+export async function checkStatusAsync(){
+    return await BackgroundFetch.getStatusAsync()
+}
+
+export async function checkIsRegistered(){
+    return await TaskManager.isTaskRegisteredAsync(HEALTH_DATA_SYNC_TASK)
+}
