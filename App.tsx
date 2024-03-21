5046410a098abcf988f33e74e062629209952fc5
prateek041 <prateeksingh9741@gmail.com>
2024-03-22 00:54:54 +0530

    Add support for native wind

--- App.tsx
+++ App.tsx
@@ -1,63 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Button } from "react-native";
-import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {
-  registerBackgroundFetchAsync,
-  unregisterBackgroundFetchAsync,
-  checkIsRegistered,
-  checkStatusAsync,
-  HEALTH_DATA_SYNC_TASK
-} from "./backgroud/healthDataSync";
 
 export default function App() {
-  const [isRegistered, setIsRegistered] = useState(false);
-
-  useEffect(() => {
-    // getHealthData();
-    check();
-  }, []);
-
-  const check = async () => {
-    const isRegistered = await checkIsRegistered();
-    console.log("isRegistered", isRegistered)
-
-    setIsRegistered(isRegistered)
-  };
-
-  const toggleFetchTask = async () => {
-    if (isRegistered) {
-      await unregisterBackgroundFetchAsync();
-    } else {
-      await registerBackgroundFetchAsync();
-    }
-
-      check();
-  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app I don't know</Text>
-      <StatusBar style="auto" />
-      <Text>Background Fetch task name: {' '}          <Text >
-            {isRegistered ? HEALTH_DATA_SYNC_TASK: 'Not registered yet!'}
-          </Text></Text>
-      <Button
-        title={
-          isRegistered
-            ? "Unregister BackgroundFetch task"
-            : "Register BackgroundFetch task"
-        }
-        onPress={toggleFetchTask}
-      />
+    <View  className="flex-1">
+      <Text>Open what up App.tsx to start working on your app I don't know</Text>
+      <StatusBar />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
