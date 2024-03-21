f1727751a83727d738627948b06adbfa36f34a55
prateek041 <prateeksingh9741@gmail.com>
2024-03-22 03:09:09 +0530

    Add new fonts and dependencies

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView} from "react-native-safe-area-context";
+import {useFonts} from "expo-font"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Mono": require("./assets/fonts/Roboto_Mono/RobotoMono-VariableFont_wght.ttf"),
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
-    <View  className="flex-1">
-      <Text>Open what up App.tsx to start working on your app I don't know</Text>
-      <StatusBar />
-    </View>
+      <SafeAreaView className="bg-[#1A1A1A] h-full" >
+        <View className="h-full mx-5 " >
+          <Text className="text-white text-lg" style={{fontFamily: "Roboto-Mono"}}>
+            Open what up App.tsx to start working on your app I don't know
+          </Text>
+          <StatusBar />
+        </View>
+      </SafeAreaView>
   );
 }
-
